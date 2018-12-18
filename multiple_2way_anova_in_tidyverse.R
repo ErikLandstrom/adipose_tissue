a617c4e70292e94641cd0f1dde6170432408df6a
Erik Ländström <erik.landstrom@hotmail.se>
2018-12-18 10:37:59 +0100

    Removed eval(as.name(factor2)), it worked but in the output returned eval(as.name(factor2)) as a variable name. Works for the adipose dataset now

--- multiple_2way_anova_in_tidyverse.R
+++ multiple_2way_anova_in_tidyverse.R
@@ -9,17 +9,17 @@
 # using a tidy tibble in a longformat as input. Outputs a tibble with the 
 # the ANOVA statistics using the tidy function from the broom package.
 
+# Save output like "name" <- function() {}
+
 
 # Arguments ---------------------------------------------------------------
 
 # tb = tidy tibble
-# factor1 = first independent variable as character
-# factor2 = second independent variable as character
-# 
+
 
 # Function ----------------------------------------------------------------
 
-multiple_2way_anova_in_tidyverse <- function(tb, factor1, factor2) {
+multiple_2way_anova_in_tidyverse <- function(tb) {
   
   # Libraries
   library(tidyverse)
@@ -29,7 +29,7 @@ multiple_2way_anova_in_tidyverse <- function(tb, factor1, factor2) {
   anova <- tb %>%
     group_by(name) %>%
     nest() %>%
-    mutate(anova = map(data, ~ aov(LFQ ~ eval(as.name(factor1)) * eval(as.name(factor2)), data = .x)),
+    mutate(anova = map(data, ~ aov(LFQ ~ genotype * tissue, data = .x)),
            tidied = map(anova, tidy)) %>%
     unnest(tidied)
   return(anova)
