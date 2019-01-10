53e0f012c43f69794e18b08734b19eab937106f5
Erik Ländström <erik.landstrom@hotmail.se>
2019-01-10 12:39:09 +0100

    Changed the interaction parameter

---
+++ multiple_2way_anova_in_tidyverse_with_no_interaction.R
@@ -0,0 +1,39 @@
+### multiple_2way_anova_in_tidyverse_with_no_interaction
+### Author: Erik Ländström
+### Date: 181217
+
+
+# Description -------------------------------------------------------------
+
+# Performs a two way ANOVA for each feature of the dataset (e.g. protein),
+# using a tidy tibble in a longformat as input. Outputs a tibble with the 
+# the ANOVA statistics using the tidy function from the broom package.
+# Changed original function to 2-way ANOVA with no interaction.
+
+# Save output like "name" <- function() {}
+
+# Use unnest(tidy_.+) to extract p-values
+
+
+# Arguments ---------------------------------------------------------------
+
+# tb = tidy tibble
+
+
+# Function ----------------------------------------------------------------
+
+multiple_2way_anova_in_tidyverse_with_no_interaction <- function(tb) {
+  
+  # Libraries
+  library(tidyverse)
+  library(broom)
+  
+  # 2-way ANOVA
+  anova <- tb %>%
+    group_by(name) %>%
+    nest() %>%
+    mutate(anova = map(data, ~ aov(LFQ ~ genotype + tissue, data = .x)),
+           tidy_anova = map(anova, tidy))
+  
+  return(anova)
+}
