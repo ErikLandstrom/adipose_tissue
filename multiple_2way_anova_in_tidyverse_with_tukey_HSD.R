fed2f7c772359ab557466e95d0adbe3b0f9d48ba
Erik Ländström <erik.landstrom@hotmail.se>
2019-01-11 14:36:46 +0100

    The function is now updated to be more general, it should now work with any kind of data

--- multiple_2way_anova_in_tidyverse_with_tukey_HSD.R
+++ multiple_2way_anova_in_tidyverse_with_tukey_HSD.R
@@ -18,23 +18,37 @@
 # Arguments ---------------------------------------------------------------
 
 # tb = tidy tibble
-
+# cases = column containing the name of the cases
+# observations = string, column containing dependent variable
+# independent_var1 = string, column containing independent factor 1
+# independent_var2 = string, column containing independent factor 2
 
 # Function ----------------------------------------------------------------
 
-multiple_2way_anova_in_tidyverse_with_tukeyHSD <- function(tb) {
+multiple_2way_anova_in_tidyverse_with_tukeyHSD <- function(tb, cases, observations,
+                                                           independent_var1,
+                                                           independent_var2) {
   
   # Libraries
   library(tidyverse)
   library(broom)
   
+  # Quoting
+  cases <- enquo(cases)
+  
+  # Create formula object
+  lm_formula <- as.formula(paste(observations, "~", independent_var1, "*", independent_var2))
+  
+  # Create string with the name of the interaction effect
+  interaction_effect <- paste(independent_var1, ":", independent_var2, sep = "")
+  
   # 2-way ANOVA
   anova <- tb %>%
-    group_by(name) %>%
+    group_by(!!cases) %>%
     nest() %>%
-    mutate(anova = map(data, ~ aov(LFQ ~ genotype * tissue, data = .x)),
-           tukey = map(data, ~ TukeyHSD(aov(LFQ ~ genotype * tissue, data = .x),
-                                        which = "genotype:tissue")),
+    mutate(anova = map(data, ~ aov(lm_formula, data = .x)),
+           tukey = map(data, ~ TukeyHSD(aov(lm_formula, data = .x),
+                                        which = interaction_effect)),
            tidy_anova = map(anova, tidy),
            tidy_tukey = map(tukey, tidy))
   
