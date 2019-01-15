62de150bf3515f529c0ebe392b7e38b251363f08
Erik Ländström <erik.landstrom@hotmail.se>
2019-01-15 17:13:59 +0100

    Changed name of function

--- join_qvalues_with_results.R
+++ join_qvalues_with_results.R
@@ -1,4 +1,4 @@
-### join_qvalues_with_ANOVA_results
+### join_qvalues_with_results
 ### Author: Erik Ländström
 ### Date: 190110
 
@@ -14,15 +14,16 @@
 # tb = output from fdr_correction_with_qvalue.R
 
 
+
 # Function ----------------------------------------------------------------
 
-join_qvalues_with_ANOVA_results <- function(tb) {
+join_qvalues_with_results <- function(tb, p_value_col = "p_value") {
   
   # Make a tibble with p and qvalues
   qvalues <- tibble(p_value = qvalues[["pvalues"]], qvalue = qvalues[["qvalues"]])
   
   # Join with ANOVA results
-  anova_results <- full_join(anova_results, qvalues, by = "p.value")
+  tb <- full_join(tb, qvalues, by = p_value_col)
   
-  return(anova_results)
+  return(tb)
 }
