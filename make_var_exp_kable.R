3ba14406ee3954295f59b620b446690cd1e5161d
Erik Ländström <erik.landstrom@hotmail.se>
2019-01-24 16:03:28 +0100

    First

---
+++ make_var_exp_kable.R
@@ -0,0 +1,34 @@
+### make_var_exp_kable
+### Author: Erik Ländström
+### Date: 190124
+
+
+# Description -------------------------------------------------------------
+
+# Prints a variance explained table using kable.
+
+
+# Arguments ---------------------------------------------------------------
+
+# tb = pca results
+# print_format = character, specifying which format to use. Uses the default
+# formats from the kable function
+
+
+# Function ----------------------------------------------------------------
+
+make_var_exp_kable <- function(tb, print_format) {
+  
+  library(knitr)
+  
+  kable(
+    tb, 
+    format    = print_format,
+    digits    = 4,
+    col.names = c("PC", "Variance",
+                  "Proportion explained", 
+                  "Cumulative proportion explained"),
+    align     = "c",
+    caption   = "Proportion of the variance that each principal component explains"
+  )
+}
