fd6291006fbc8015a1ed8eacfadbe4dd4ea8db2a
Erik Ländström <erik.landstrom@hotmail.se>
2019-01-28 12:01:46 +0100

    Added input parameters for group names, mean column names and an option to choose which column the observations are in

--- multiple_ttests.R
+++ multiple_ttests.R
@@ -14,37 +14,49 @@
 # Arguments ---------------------------------------------------------------
 
 # tb = tidy tibble in long format
-# col_name = column name of the variable of interest, unquoted
+# group_var = column name of the variable of interest, unquoted
+# values = column name of observations
 # equal_var = logical, if equal variance should be assumed
 
 # Function ----------------------------------------------------------------
 
-multiple_ttests <- function(tb, col_name, equal_var = FALSE) {
+multiple_ttests <- function(tb, group_var, 
+                            group_1,
+                            group_2,
+                            mean_1,
+                            mean_2,
+                            values = LFQ, 
+                            equal_var = FALSE) {
   
   # Libraries
   library(tidyverse)
   library(broom)
   
   # Quote
-  col_name <- enquo(col_name)
+  group_var <- enquo(group_var)
+  values    <- enquo(values)
+  group_1   <- enquo(group_1)
+  group_2   <- enquo(group_2)
+  mean_1_name <- enquo(mean_1)
+  mean_2_name <- enquo(mean_2)
   
   # Calculate the mean for each protein
   means <- tb %>%
     group_by(name) %>%
-    summarise(mean_total = mean(LFQ))
+    summarise(mean_total = mean(!!values))
   
   
   # ttest
   t_test <-  tb %>%
-    dplyr::select(name, !!col_name, LFQ) %>%
-    group_by(name, !!col_name) %>%
+    dplyr::select(name, !!group_var, !!values) %>%
+    group_by(name, !!group_var) %>%
     nest() %>%
-    spread(key = !!col_name, value = data) %>%
+    spread(key = !!group_var, value = data) %>%
     group_by(name) %>%
-    mutate(p_value = t.test(unlist(midy), unlist(wt), var.equal = equal_var)$p.value,
-           mean_midy = mean(unlist(midy)),
-           mean_wt = mean(unlist(wt)),
-           log2_difference = mean_midy - mean_wt)
+    mutate(p_value = t.test(unlist(!!group_1), unlist(!!group_2), var.equal = equal_var)$p.value,
+           !!mean_1_name := mean(unlist(!!group_1)),
+           !!mean_2_name := mean(unlist(!!group_2)),
+           log2_difference = !!mean_1_name - !!mean_2_name)
   
   # Join data sets
   t_test <- left_join(t_test, means, by = "name")
