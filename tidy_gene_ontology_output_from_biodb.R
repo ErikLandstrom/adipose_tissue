fbb30adc0d6a9fe786c7af50509898edacf50bf1
Erik Ländström <erik.landstrom@hotmail.se>
2019-02-06 12:42:33 +0100

    First

---
+++ tidy_gene_ontology_output_from_biodb.R
@@ -0,0 +1,68 @@
+### tidy_gene_ontology_output_from_biodb
+### Author: Erik Ländström
+### Date: 190206
+
+
+# Description -------------------------------------------------------------
+
+# Tidies and cleans up annotation data from biodb. Input data should be in the
+# format:
+
+# gene_name, with each gene ontology type in a separated column.
+
+# The scripts works with the three gene ontology categories and KEGG pathways
+# (KEGG pathway info in biodb, maybe othe KEGG options work too). But the script
+# can easily be modified to handle more or less categories (although there could
+# be a need to update regex expressions then)
+
+# Save output with: object_name <- function()
+
+
+# Arguments ---------------------------------------------------------------
+
+# tb = tibble, gene names with annotation data from biodb
+
+
+# Function ----------------------------------------------------------------
+
+tidy_gene_ontology_output_from_biodb <- function(tb) {
+  
+  # Libraries
+  library(dplyr)
+  library(stringr)
+  library(tidyr)
+  
+  # Tidy data
+  ontologies <- tb %>% 
+    rename(gene_name = `Gene Symbol`, 
+           bp = `GO - Biological Process`,
+           cc =`GO - Cellular Component`,
+           mf = `GO - Molecular Function`,
+           kegg = `KEGG Pathway Info`) %>%
+    gather(bp, cc, mf, kegg, key = "type", value = "annotation") %>% # gathers all annotations into one column
+    group_by(gene_name, type) %>% 
+    nest() %>% 
+    mutate(data = str_split(data, ";")) %>% # Separates all annotations per gene
+    unnest() %>% 
+    separate(data, into = c("annotation_id", "annotation"), sep = " \\[") %>% 
+    ungroup() %>%  
+    group_by(gene_name, type) %>% 
+    distinct(annotation_id, .keep_all = TRUE) # Removes duplicated annotations per gene that for some reason gets created
+  
+  # Clean up strings in annotaion_id column (identifiers)
+  ontologies$annotation_id <- str_replace_all(ontologies$annotation_id, 'list\\(annotation = \\"', "")
+  ontologies$annotation_id <- str_replace_all(ontologies$annotation_id, 'list\\(annotation = c\\(\\"', "")
+  ontologies$annotation_id <- str_replace_all(ontologies$annotation_id, '.*\\"\\)', "")
+  
+  # Clean up strings in annotation column (descriptions)
+  ontologies$annotation <- str_replace_all(ontologies$annotation, "Name: ", "")
+  ontologies$annotation <- str_replace_all(ontologies$annotation, "\\].*", "")
+  
+  # Replace empty strings and ")" with NA in annotation_id column
+  ontologies <- ontologies %>% 
+    mutate(annotation_id = ifelse(annotation_id == "", NA, annotation_id),
+           annotation_id = ifelse(annotation_id == ")", NA, annotation_id)) 
+  
+  # Return object
+  return(ontologies)
+}
