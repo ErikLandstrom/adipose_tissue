ac70c094238806cc323a649b4545d6294eba81d8
Erik Ländström <erik.landstrom@hotmail.se>
2019-03-08 11:52:14 +0100

    Added argument to make function more general and updated the annotations

--- separate_condition_from_DEP_output.R
+++ separate_condition_from_DEP_output.R
@@ -1,10 +1,23 @@
-adipose_imputed <- adipose_imputed %>%
-  separate(condition, into = c("tissue", "genotype"), sep = "_") %>%
-  rename(LFQ = intensity)
+### separate_condition_from_DEP_output
+### Author: Erik Ländström
+### Date: 190110
 
-separate_condition_from_DEP_output <- function (tb) {
+
+# Description -------------------------------------------------------------
+
+# Separates the condition column from a DEP output into separate factors.
+
+
+# Arguments ---------------------------------------------------------------
+
+# tb = tibble, DEP output long fromat
+# conditions = character vector, column names for the new factors
+
+# Function ----------------------------------------------------------------
+
+separate_condition_from_DEP_output <- function (tb, conditions) {
   temp <- tb %>% 
-    separate(condition, into = c("tissue", "genotype"), sep = "_") %>% 
+    separate(condition, into = conditions, sep = "_") %>% 
     rename(LFQ = intensity)
   return(temp)
 }
